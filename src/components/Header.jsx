3832dc5aa979fc962cc1eb54de4e3e25dd093180
adel <adeelguitoun@gmail.com>
2023-11-08 17:34:44 +0100

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../utils/AuthContext';
 
 const Header = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { user } = useAuth();
 
-    const logoutClick = () => {
-        navigate('/login')
-    }
+  const logoutClick = () => {
+    navigate('/login');
+  };
 
   return (
     <div className="header">
-        <div>
-            <Link id="header-logo" to="/">LOGO</Link>
-        </div>
+      <div>
+        <Link id="header-logo" to="/">
+          LOGO
+        </Link>
+      </div>
 
-        <div className="links--wrapper">
-            <>
-                <Link to="/" className="header--link">Home</Link>
-                <Link to="/profile" className="header--link">Profile</Link>
+      <div className="links--wrapper">
+        {user ? (
+          <>
+            <Link to="/" className="header--link">
+              Home
+            </Link>
+            <Link to="/profile" className="header--link">
+              Profile
+            </Link>
 
-                <button onClick={logoutClick} className="btn">Logout</button>
-            </>
-            {/* <>
-
-                <Link className="btn" to="/login">Login</Link>
-            </> */}
-            
-        </div>
+            <button onClick={logoutClick} className="btn">
+              Logout
+            </button>
+          </>
+        ) : (
+          <Link className="btn" to="/login">
+            Login
+          </Link>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
