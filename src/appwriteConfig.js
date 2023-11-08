2cf34429085fd82ec6a26220d53fdd0360fc48dc
adel <adeelguitoun@gmail.com>
2023-11-08 21:37:59 +0100

    Add  appwrite account

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,11 +1,12 @@
-import { Client } from 'appwrite';
-
-const client = new Client();
+import { Account, Client } from 'appwrite';
 
 const appwriteConfig = {
   url: import.meta.env.VITE_APPWRITE_URL,
   key: import.meta.env.VITE_APPWRITE_KEY,
 };
+
+const client = new Client();
+export const account = new Account(client);
 
 client.setEndpoint(appwriteConfig.url).setProject(appwriteConfig.key);
 
