119e4b84b7b8a49e31230f152845c99780a6470e
adel <adeelguitoun@gmail.com>
2023-11-08 21:40:20 +0100

    Update home route

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
